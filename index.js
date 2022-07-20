ee97a3af81facd66f8034e877f616c23695cdd82
Jose hernandez <hernandez.jose85@gmail.com>
2022-07-20 10:15:04 -0400

    Probando node-cron
    Prueba para hacer un cron-job en el index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-(async () => {
+// npm i uuid
+// npm i node-cron
+// https://crontab.guru/
+// https://support.acquia.com/hc/en-us/articles/360004224494-Cron-time-string-format
+// https://www.digitalocean.com/community/tutorials/nodejs-cron-jobs-by-examples
+// https://www.npmjs.com/package/node-cron
+import nodecron from 'node-cron';
 
-  process.exit(0);
-})();
+// Importación del programa principal que hace la gestion de consultas API, calculos y actualizacion de la Base de Datos.
+import Principal from "./calculos.js";
+
+// Cada 15 segundos se hace la actualizacion de la base de datos
+nodecron.schedule('*/15 * * * * *', () => {
+  console.log('Cada 15 Segundos se ejecuta esto');
+  Principal();
+});
